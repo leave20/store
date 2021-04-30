7eb7573c0c9353827c8d4170e92cc2c68eda09dc
alexTexis <alextexis.js@gmail.com>
2021-04-30 01:10:41 -0500

    feat: creafts section

--- src/components/Catalogo/index.js
+++ src/components/Catalogo/index.js
@@ -1,6 +1,8 @@
-import { makeStyles, Box, Container, Typography } from "@material-ui/core";
+import { makeStyles, Box, Container, Link } from "@material-ui/core";
 
 import { Carousel } from "./Carousel";
+import SectionHeader from "../SectionHeader";
+
 // import newTheme from '../styles/NewTheme';
 
 const mockData = [
@@ -53,19 +55,8 @@ const useStyles = makeStyles((theme) => ({
     margin: "24px 0",
     overflow: "hidden",
   },
-  headingBox: {
-    textAlign: "center",
-    marginBottom: 24,
-  },
-  heading: {
-    fontWeight: 700,
-    marginBottom: 38,
-    color: "#212834",
-    fontFamily: "fantasy",
-  },
   anchor: {
     textDecoration: "none",
-    color: "#212834",
   },
 }));
 
@@ -75,19 +66,14 @@ export default function Catalogo({ title, dark, id }) {
   return (
     <Box component="section" className={classes.root}>
       <Container>
-        <Box className={classes.headingBox}>
-          <Typography variant="h4" className={classes.heading}>
-            {title}
-          </Typography>
-          <Typography
-            component="a"
-            variant="body2"
-            href="/"
-            className={classes.anchor}
-          >
-            {"< VER TODOS >"}
-          </Typography>
-        </Box>
+        <SectionHeader
+          title={title}
+          descriptionElement={(props) => (
+            <Link href="/" className={`${props.className} ${classes.anchor}`}>
+              {"< VER TODOS >"}
+            </Link>
+          )}
+        />
         <Carousel mb={120} data={mockData} />
         <Carousel data={mockData} />
       </Container>
