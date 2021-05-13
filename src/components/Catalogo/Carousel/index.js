26b8a70c9f85d454b7d065c8c54d425d8442919c
unknown <sebastian.fariasb@usach.cl>
2021-05-13 17:11:43 -0400

    Working ok

--- src/components/Catalogo/Carousel/index.js
+++ src/components/Catalogo/Carousel/index.js
@@ -1,9 +1,9 @@
 import Slider from "react-slick";
 import { Slide } from "../Slide";
 
-export const Carousel = ({ data = [], mb = 0 }) => (
+export const Carousel = ({ data = [], mb = 0, mobile }) => (
   <div style={{ margin: "0 -13px", marginBottom: mb }}>
-    <Slider slidesToShow={4}>
+    <Slider slidesToShow={mobile ? 2 : 4}>
       {Array.isArray(data) &&
         data.map((item) => <Slide key={item.id} {...item} />)}
     </Slider>
--- src/components/Catalogo/index.js
+++ src/components/Catalogo/index.js
@@ -1,5 +1,11 @@
-import { makeStyles, Box, Container, Link } from "@material-ui/core";
-
+import { useEffect, useState } from "react";
+import {
+  makeStyles,
+  Box,
+  Container,
+  Link,
+  useMediaQuery,
+} from "@material-ui/core";
 import { Carousel } from "./Carousel";
 import SectionHeader from "../SectionHeader";
 
@@ -61,21 +67,33 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Catalogo({ title, dark, id }) {
+  const getMobile = () => {
+    // return useMediaQuery( theme => theme.breakpoint.down('sm')); usar esto cuando haya Theme Provider
+    return window.innerWidth < 960;
+  };
   const classes = useStyles();
+  const [isMobile, setIsMobile] = useState(getMobile());
 
+  useEffect(() => {
+    const handleResize = (e) => {
+      setIsMobile(getMobile());
+    };
+    window.addEventListener("resize", handleResize);
+    return window.removeEventListener("rezise", handleResize);
+  }, []);
   return (
-    <Box component="section" className={classes.root}>
+    <Box component="section" className={classes.root} id={id}>
       <Container>
         <SectionHeader
           title={title}
           descriptionElement={(props) => (
             <Link href="/" className={`${props.className} ${classes.anchor}`}>
-              {"< VER TODOS >"}
+              {`isMobile: ${isMobile}`}
             </Link>
           )}
         />
-        <Carousel mb={120} data={mockData} />
-        <Carousel data={mockData} />
+        <Carousel mb={120} mobile={isMobile} data={mockData} />
+        <Carousel mobile={isMobile} data={mockData} />
       </Container>
     </Box>
   );
